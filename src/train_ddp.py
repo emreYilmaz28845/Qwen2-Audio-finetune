e734fb22120bbdca3fa2d6696fd9ac40603557dc
emreYilmaz28845 <emre.yilmaz.28845@ozu.edu.tr>
2026-03-26 19:00:20 +0300

    returning back :(

--- src/train_ddp.py
+++ src/train_ddp.py
@@ -11,7 +11,6 @@ import math
 from torch.nn.parallel import DistributedDataParallel as DDP
 import torch
 from torch.optim import lr_scheduler
-from transformers.modeling_outputs import BaseModelOutput
 from utils.set_logger import set_logger
 from utils.set_seed import set_seed
 from utils.init_process import setup_ddp
@@ -75,27 +74,30 @@ def create_modified_qwen2audio_encoder(original_encoder, adapter_config):
         output_hidden_states=None,
         return_dict=None,
     ):
-        if return_dict is None:
-            return_dict = self.config.use_return_dict
-
         outputs = original_forward(
             input_features=input_features,
             attention_mask=attention_mask,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
-            return_dict=True,
-        )
-
-        adapted_audio_features = adapter(outputs.last_hidden_state)
-        adapted_outputs = BaseModelOutput(
-            last_hidden_state=adapted_audio_features,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions,
+            return_dict=return_dict,
         )
 
         if return_dict:
-            return adapted_outputs
-        return adapted_outputs.to_tuple()
+            audio_features = outputs.last_hidden_state
+        else:
+            audio_features = outputs[0]
+
+        adapted_audio_features = adapter(audio_features)
+
+        if return_dict:
+            from ...modeling_outputs import BaseModelOutput
+            return BaseModelOutput(
+                last_hidden_state=adapted_audio_features,
+                hidden_states=outputs.hidden_states,
+                attentions=outputs.attentions,
+            )
+        else:
+            return (adapted_audio_features,) + outputs[1:]
 
     original_encoder.forward = new_forward.__get__(original_encoder, type(original_encoder))
     original_encoder.audio_adapter = adapter
@@ -161,7 +163,8 @@ def train_ddp(cfg):
     peft_cfg["target_modules"] = list(peft_cfg["target_modules"])
     peft_cfg = LoraConfig(**peft_cfg)
 
-    model.language_model = get_peft_model(model.language_model, peft_cfg)
+    #model = get_peft_model(model.language_model, peft_cfg)
+    model = get_peft_model(model, peft_cfg)
 
     # ===============================
     # Enable gradient checkpointing to reduce activation memory
